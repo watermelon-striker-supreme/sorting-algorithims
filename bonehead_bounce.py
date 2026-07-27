12c27881c585ecf1f6df34779178003c2c788e42
watermelon.striker.supreme <68917976+watermelon-striker-supreme@users.noreply.github.com>
2026-07-27 15:48:23 -0700

    Add files via upload
    New and improved bonehead bounce. bounces back and forth. the two if n==1:'s arent redundant. See for yourself.

---
+++ bonehead_bounce.py
@@ -0,0 +1,43 @@
+from matplotlib import pyplot as plt
+import random
+
+quantity = 20
+n = 0
+data = random.sample(range(1, quantity+1), quantity)
+x_values = range(1, len(data) + 1)
+plt.ion()
+
+while True:
+
+    plt.clf()
+    colors = ['b'] * quantity
+    colors[n] = 'r'
+    plt.bar(x_values, data, color=colors)
+    plt.xticks(range(1, len(data) + 1))
+    plt.yticks(range(0, len(data)+ 1, 5))
+    plt.pause(0.01)
+
+    if n == quantity-1:
+        break
+
+    if data[n] > data[n+1]:
+        z = data[n] - data[n+1]
+        data[n+1] += z
+        data[n] -= z
+        while True:
+            if n == 0:
+                break
+            n -= 1
+            if n == 0:
+                break
+            if data[n] < data[n-1]:
+                x = data[n-1] - data[n]
+                data[n-1] -= x
+                data[n] += x
+            else:
+                break
+    else:
+        n += 1
+
+plt.ioff()
+plt.show()
